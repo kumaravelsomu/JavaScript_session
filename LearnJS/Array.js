67996fbff12041841937f3972c1054f73357fad9
kumaravelsomu <107678851+kumaravelsomu@users.noreply.github.com>
2023-10-17 04:39:59 +0000

    updated

---
+++ LearnJS/Array.js
@@ -0,0 +1,85 @@
+// const myArray = [0, 1, 2, 3, 4, 5,"piyush","2288hhhhxxx",true]
+
+// console.log(myArray);
+
+ 
+
+// const mysecArray = ["hello", "hi"]
+
+ 
+
+// const mythrdArray = new Array(6, 7, 8, 9)
+
+ 
+
+// console.log(myArray[1]);
+
+// myArray.push(6)
+
+// console.log(myArray);
+
+// myArray.pop()
+
+// console.log(myArray);
+
+// myArray.unshift(9)
+
+// console.log(myArray);
+
+ 
+
+// myArray.shift()
+
+// console.log(myArray);
+
+ 
+
+// // includes
+
+// console.log(myArray.includes(9));
+
+ 
+
+// console.log(myArray.indexOf(0));
+
+ 
+
+// // Slice
+
+// console.log(myArray.slice(0,2));
+
+ 
+
+// console.log(myArray.splice(0,5));
+
+ 
+
+// // More array functions
+
+ 
+
+let yourArray=['a', 'b', 'c']
+
+let yourArray2=['j','k','l']
+
+ 
+
+// yourArray.push(yourArray2)
+
+ 
+
+console.log(yourArray);
+
+console.log(yourArray[3]);
+
+console.log(yourArray[3][1]);
+
+
+
+yourArray=['a', 'b', 'c']
+
+yourArray2=['j','k','l']
+
+const spreadOutArray = [...yourArray, ...yourArray2]
+
+console.log(spreadOutArray);
--- LearnJS/MyFirstJS.js
+++ LearnJS/MyFirstJS.js
@@ -1,1 +1,78 @@
-console.log('Kumar')
+// console.log('Kumar');
+// console.log(undefined==0);
+
+// //  + - * %
+
+ 
+
+// var num1 = 2
+
+// var num2 = 3
+
+ 
+
+// console.log( num1 + num2)
+
+ 
+
+// var answer = num1 > num2
+
+ 
+
+// console.log(answer)
+
+ 
+
+ 
+
+// // More calculations
+
+ 
+
+// var sellingprice = 199;
+
+// var listingprice = 799;
+
+ 
+
+// var discountprice =  ((listingprice - sellingprice)/ listingprice) * 100;
+
+ 
+
+// console.log("Discount Price : "+discountprice)
+
+ 
+
+// // Rounding up using a given function Math
+
+ 
+
+// console.log("Displaying the rounded up value: "+ Math.round(discountprice));
+
+// const stringVal= "Piyush"
+
+// const nullValue = null;
+
+// let notDefined;
+
+// let userOne ={
+
+//     email:"someone@google.com",
+
+//     phone:"9922001100"
+
+// }
+
+
+// let userTwo = userOne
+// userTwo.email = "piyush@google.com"
+
+// console.log(userOne.email);
+
+// console.log(userTwo.email);
+
+const name = "Kumar"
+
+const dept = "QA"
+
+console.log(`Hello my name is ${name} and I work as ${dept}`)
